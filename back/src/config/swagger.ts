8e950d3b7a47eefb52902e5eeb18a7159c3de112
Manuel Barrios <manuelbarrios7891238520@gmail..com>
2026-07-17 08:36:15 -0500

    fix routes dockert

--- back/src/config/swagger.ts
+++ back/src/config/swagger.ts
@@ -1,13 +1,13 @@
 import swaggerJsdoc from "swagger-jsdoc";
-import path from "path";
+
 
 const options: swaggerJsdoc.Options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "API Gestión de Productos AssisPrex ",
+      title: "API Gestión de Productos AssisPrex",
       version: "1.0.0",
-      description: "API REST para gestión de productos - prueba tecnica",
+      description: "API REST para gestión de productos - prueba técnica",
     },
     servers: [
       {
@@ -15,9 +15,13 @@ const options: swaggerJsdoc.Options = {
       },
     ],
   },
-  apis: [path.resolve("src/routes/*.ts"), path.resolve("dist/routes/*.js")],
+
+  apis: [
+    // "./src/routes/*.ts",
+    // "./dist/routes/*.js",
+    "src/routes/*.ts",
+    "dist/src/routes/*.js",
+  ],
 };
 
-const swaggerSpec = swaggerJsdoc(options);
-
-export default swaggerSpec;
+export default swaggerJsdoc(options);
